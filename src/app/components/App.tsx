e086ca137517356a128c44a90329389b0f0d034c
Elizaveta Svitanko <lizasvit@gmail.com>
2020-12-22 14:16:52 +0300

    react ds plugin vol.1

---
+++ src/app/components/App.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import '../styles/ui.css';
+import {Config, ConfigForm} from './configForm';
+import {useState} from 'react';
+
+const NETWORK_ERROR = 'Network error';
+const LAST_SUCCESSFUL_STATUS = 299;
+
+function httpRequest(config: Config, requestType, url, setErrorLog, setLoading, content?) {
+    return new Promise(function(resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(requestType, url);
+        xhr.setRequestHeader('Authorization', `token ${config.token}`);
+        xhr.responseType = 'text';
+
+        xhr.onload = function() {
+            if (xhr.status > LAST_SUCCESSFUL_STATUS) {
+                setLoading(false);
+                setErrorLog(
+                    `Ошибка в запросе ${requestType} ${url}: ${xhr.status} ${JSON.parse(xhr.responseText).message}`,
+                );
+                reject();
+            } else {
+                resolve(this.response);
+            }
+        };
+
+        xhr.onerror = function() {
+            setLoading(false);
+            setErrorLog(`Ошибка в запросе ${requestType} ${url}: ${NETWORK_ERROR}`);
+            reject(new Error(NETWORK_ERROR));
+        };
+
+        content ? xhr.send(content) : xhr.send();
+    });
+}
+
+function createEmptyConfig() {
+    return {
+        repoPath: '',
+        token: '',
+        committerName: '',
+        committerEmail: '',
+        headBranch: '',
+        baseBranch: '',
+    };
+}
+
+const App = ({}) => {
+    let [cachedConfig, setCachedConfig] = useState(createEmptyConfig());
+    const [isLoading, setLoading] = useState(false);
+    const [errorLog, setErrorLog] = useState('');
+    const [successLog, setSuccessLog] = useState('');
+
+    React.useEffect(() => {
+        parent.postMessage({pluginMessage: {type: 'getConfig'}}, '*');
+        window.onmessage = async (event) => {
+            if (event.data.pluginMessage.type === 'networkRequest') {
+                setLoading(true);
+                const config = event.data.pluginMessage.config;
+
+                // Get last sha of file in which to commit
+                httpRequest(
+                    config,
+                    'GET',
+                    `${config.repoPath}/contents/styles.json?ref=${config.headBranch}`,
+                    setErrorLog,
+                    setLoading,
+                )
+                    .then((response: string) => {
+                        // Commit changes to head branch
+                        const sha = JSON.parse(response).sha;
+                        return httpRequest(
+                            config,
+                            'PUT',
+                            `${config.repoPath}/contents/styles.json`,
+                            setErrorLog,
+                            setLoading,
+                            [
+                                JSON.stringify({
+                                    message: 'applying Figma styles update',
+                                    content: window.btoa(event.data.pluginMessage.content),
+                                    branch: config.headBranch,
+                                    committer: {
+                                        name: config.committerName,
+                                        email: config.committerEmail,
+                                    },
+                                    sha,
+                                }),
+                            ],
+                        );
+                    })
+                    .then(() => {
+                        // Make a pull request from head to base branch
+                        const repoNameParsed = config.repoPath.split('/');
+                        return httpRequest(config, 'POST', `${config.repoPath}/pulls`, setErrorLog, setLoading, [
+                            JSON.stringify({
+                                owner: config.committerName,
+                                repo: repoNameParsed[repoNameParsed.length - 1],
+                                head: config.headBranch,
+                                base: config.baseBranch,
+                                title: 'Update styles',
+                            }),
+                        ]);
+                    })
+                    .then(closePlugin);
+            }
+
+            if (event.data.pluginMessage.type === 'githubConfig') {
+                setCachedConfig(event.data.pluginMessage.content);
+            }
+        };
+    }, []);
+
+    function closePlugin() {
+        setLoading(false);
+        setSuccessLog('Успешно отправлено!');
+        setTimeout(() => window.parent.postMessage({pluginMessage: {type: 'done'}}, '*'), 1000);
+    }
+
+    return (
+        <div>
+            <h2>Синхронизировать дизайн с кодом?</h2>
+            <ConfigForm cachedConfig={cachedConfig}/>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : errorLog.length > 0 ? (
+                <p className="error-message">{errorLog}</p>
+            ) : (
+                <p className="success-message">{successLog}</p>
+            )}
+        </div>
+    );
+};
+
+export default App;
---
+++ src/plugin/controller.ts
@@ -0,0 +1,68 @@
+import {convertPaintColor} from '../app/utils/colorUtils';
+
+figma.showUI(__html__, {width: 600, height: 600});
+
+figma.ui.onmessage = (msg) => {
+    if (msg.type === 'setConfig') {
+        figma.clientStorage.setAsync('config', JSON.stringify(msg.config));
+    }
+    if (msg.type === 'getConfig') {
+        figma.clientStorage.getAsync('config').then((config) => {
+            figma.ui.postMessage({type: 'githubConfig', content: JSON.parse(config)});
+        });
+    }
+    if (msg.type === 'send') {
+        figma.clientStorage.setAsync('config', JSON.stringify(msg.config));
+        let styles = {};
+
+        // Get text styles changes
+        styles['textStyles'] = figma.getLocalTextStyles().map((style) => {
+            return {
+                [style.name]: {
+                    fontName: style.fontName,
+                    fontSize: style.fontSize,
+                    textCase: style.textCase,
+                    textDecoration: style.textDecoration,
+                    letterSpacing: style.letterSpacing,
+                    lineHeight: style.lineHeight,
+                },
+            };
+        });
+
+        // Get colors
+        // TODO: get necessary properties when they'll be in figma
+        styles['colorStyles'] = figma.getLocalPaintStyles().map((style) => {
+            return {
+                [style.name]: {
+                    paints: style.paints.map((paint) => convertPaintColor(paint)),
+                },
+            };
+        });
+
+        // Get svg icons (mostly for Web React now)
+        let nodes = figma.currentPage.findAll((node) => node.type === 'VECTOR');
+        let iconStyles = [];
+        for (let node of nodes) {
+            if ('vectorPaths' in node) {
+                iconStyles.push({
+                    name: node.name,
+                    path: node.vectorPaths,
+                    paints: node.fills?.map((fill: any) => convertPaintColor(fill)),
+                    width: node.width,
+                    height: node.height,
+                });
+            }
+        }
+        styles['iconStyles'] = iconStyles;
+
+        // Transfer styles to ui for a network request to Github
+        figma.ui.postMessage({
+            type: 'networkRequest',
+            content: JSON.stringify(styles, null, 2),
+            config: msg.config,
+        });
+    }
+    if (msg.type === 'done') {
+        figma.closePlugin();
+    }
+};
